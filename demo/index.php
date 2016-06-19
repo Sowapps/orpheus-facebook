ff1892f0c42f848e2833744c62f2169ac103a1be
Loenix <florent@sowapps.com>
2016-06-19 18:20:05 +0200

    Implement Facebook Service for API v5 Add a demo page to test login

---
+++ demo/index.php
@@ -0,0 +1,262 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+session_start();
+
+define('VENDORPATH',	dirname(__DIR__).'/vendor');
+
+// echo VENDORPATH.'/autoload.php<br>';
+if( !file_exists(VENDORPATH.'/autoload.php') ) {
+	throw new Exception('Unable to find vendor autoload, check composer.');
+}
+$PackageLoader	= require_once VENDORPATH.'/autoload.php';
+// $PackageLoader->add('Orpheus\\Web\\Facebook\\', '../../orpheus-facebook/src/');
+ 
+// require_once '../../git/orpheus-facebook/src/';
+
+define('FACEBOOK_APP_ID',		'1000307813379593');
+define('FACEBOOK_APP_SECRET',	'46f13c2bb029dfee1becb6739d5234e1');
+
+use Orpheus\Web\Facebook\FacebookService;
+
+$FBService = new FacebookService();
+// $fb = new Facebook\Facebook([
+// 		'app_id' => '{app-id}', // Replace {app-id} with your app id
+// 		'app_secret' => '{app-secret}',
+// 		'default_graph_version' => 'v2.2',
+// ]);
+
+
+$SelfURL = 'http://testfb.orpheus-framework.com/index.php';
+
+if( !empty($_GET['logout']) ) {
+	$FBService->disconnectUser();
+	header('Location: '.$SelfURL);
+	die();
+}
+
+$accessToken = $FBService->getAccessToken();
+
+// if( isset($_SESSION['fb_access_token']) ) {
+// 	$accessToken = $_SESSION['fb_access_token'];
+	
+// } else {
+if( !$accessToken ) {
+	// Check facebook login
+	try {
+		if( $FBService->connectUser() ) {
+			header('Location: '.$SelfURL);
+			die();
+		}
+		
+		/*
+		$accessToken = $helper->getAccessToken();
+// 		echo 'FB Accesstoken => '.$accessToken.'<br>';
+	
+		if( $accessToken ) {
+			// Logged in
+	// 		echo '
+	// <h3>Facebook was just logged in</h3>
+	// <h6>Access Token</h6>';
+	// 		var_dump($accessToken->getValue());
+		
+			// The OAuth 2.0 client handler helps us manage access tokens
+			$oAuth2Client = $FBService->getOAuth2Client();
+		
+			// Get the access token metadata from /debug_token
+			$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+	// 		echo '
+	// <h6>Metadata</h6>';
+	// 		var_dump($tokenMetadata);
+		
+			// Validation (these will throw FacebookSDKException's when they fail)
+			$tokenMetadata->validateAppId(FACEBOOK_APP_ID); // Replace {app-id} with your app id
+			// If you know the user ID this access token belongs to, you can validate it here
+			//$tokenMetadata->validateUserId('123');
+			$tokenMetadata->validateExpiration();
+		
+			if( !$accessToken->isLongLived() ) {
+// 				echo 'Access token is not a long lived<br />';
+				// Exchanges a short-lived access token for a long-lived one
+				try {
+					$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+// 					echo 'Got long lived access token<br />';
+				} catch (Facebook\Exceptions\FacebookSDKException $e) {
+					echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+					exit;
+				}
+		
+	// 			echo '
+	// <h6>Long-lived</h6>';
+	// 			var_dump($accessToken->getValue());
+// 			} else {
+// 				echo 'Access token IS a long lived<br>'.$accessToken.'<br>';
+			}
+			$_SESSION['fb_access_token'] = (string) $accessToken;
+			header('Location: '.$SelfURL);
+// 			header('Location: index.php');
+			die();
+		}
+		*/
+		
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: '.$e;
+// 		exit;
+		
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: '.$e;
+// 		exit;
+	}
+}
+
+// $helper = $FBService->getFacebook()->getRedirectLoginHelper();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Orpheus Facebook</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css" />
+	
+</head>
+<body>
+	<div class="container">
+	
+<?php
+
+
+/*
+if( $accessToken ) {
+	// Logged in
+	echo '
+<h1>Facebook session found</h3>
+<h3>Access Token</h3>';
+	var_dump($accessToken->getValue());
+	
+	// The OAuth 2.0 client handler helps us manage access tokens
+	$oAuth2Client = $FBService->getOAuth2Client();
+	
+	// Get the access token metadata from /debug_token
+	$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+	echo '
+<h3>Metadata</h3>';
+	var_dump($tokenMetadata);
+	
+	// Validation (these will throw FacebookSDKException's when they fail)
+	$tokenMetadata->validateAppId(FACEBOOK_APP_ID); // Replace {app-id} with your app id
+	// If you know the user ID this access token belongs to, you can validate it here
+	//$tokenMetadata->validateUserId('123');
+	$tokenMetadata->validateExpiration();
+	
+	if (! $accessToken->isLongLived()) {
+		// Exchanges a short-lived access token for a long-lived one
+		try {
+			$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+		} catch (Facebook\Exceptions\FacebookSDKException $e) {
+			echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+			exit;
+		}
+	
+		echo '
+<h3>Long-lived</h3>';
+		var_dump($accessToken->getValue());
+	}
+}
+*/
+?>
+		
+		<h1>Facebook Login</h1>
+		<p class="lead">
+			Test for facebook login.
+			<a href="https://developers.facebook.com/docs/facebook-login/web" target="_blank">Documentation</a>
+		</p>
+		<?php
+		if( $accessToken ) {
+			echo '
+				<h3>User is logged in</h3>';
+			
+			//https://developers.facebook.com/tools/debug/accesstoken/
+		// 	echo '
+		// 		<h6>Token</h6>
+		// 		'.$accessToken.'<br>';
+			
+			try {
+				// Returns a `Facebook\FacebookResponse` object
+		// 		$response = $FBService->get('/me?fields=email,id,name', $accessToken);
+				
+		// // 		$response = $FBService->get('/me?fields=id,name', $accessToken);
+		// // 		var_dump($response);echo '<br>';
+		// 		$user = $response->getGraphUser();
+				$user = $FBService->getUser('name,picture,gender');
+// 				var_dump($user);echo '<br>';
+				$picture = $user->getPicture();
+				echo '
+				<div class="media">'.($picture ? '
+					<div class="media-left">
+						<a href="'.$picture->getUrl().'" target="_blank">
+							<img class="media-object" src="'.$picture->getUrl().'" alt="'.$user->getName().'">
+						</a>
+					</div>' : '').'
+					<div class="media-body">
+						<h4 class="media-heading">'.$user->getName().'</h4>
+						Welcome, you are logged in via Facebook.
+					</div>
+				</div><br>';
+// 				<p>Welcome '.$user->getName().', you logged in via Facebook.</p>';
+			
+			} catch(Facebook\Exceptions\FacebookResponseException $e) {
+				echo 'Graph returned an error: '.$e;
+		// 		exit;
+			} catch(Facebook\Exceptions\FacebookSDKException $e) {
+				echo 'Facebook SDK returned an error: '.$e;
+		// 		exit;
+			}
+		
+			echo '
+				<a class="btn btn-default" href="'.$SelfURL.'?logout=1"><i class="fa fa-fw fa-sign-out"></i> Log out</a>';
+		} else {
+// 		if( !$accessToken ) {
+// 			$permissions = ['email']; // Optional permissions
+// 			$loginUrl = $helper->getLoginUrl($SelfURL, $permissions);
+			$loginUrl = $FBService->getLoginUrl($SelfURL, array('email'));
+			echo '
+		<a class="btn btn-primary" href="' . htmlspecialchars($loginUrl) . '"><i class="fa fa-fw fa-facebook"></i> Log in with Facebook !</a>';
+		}
+		
+		?>
+
+	</div>
+
+<script>
+window.fbAsyncInit = function() {
+	FB.init({
+		appId	: '<?php echo FACEBOOK_APP_ID; ?>',
+		xfbml	: true,
+		version	: 'v2.6'
+	});
+};
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if( d.getElementById(id) ) {
+		return;
+	}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+</div>
+	<!-- External JS libraries -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
--- src/FacebookService.php
+++ src/FacebookService.php
@@ -1,22 +1,146 @@
 <?php
 
-use Facebook\FacebookSession;
-use Facebook\GraphUser;
-use Facebook\FacebookRequest;
-use Facebook\GraphAlbum;
-use Facebook\Entities\SignedRequest;
+namespace Orpheus\Web\Facebook;
+
+use Facebook\Facebook;
+use Facebook\GraphNodes\GraphUser;
+// use Facebook\FacebookSession;
+// use Facebook\GraphUser;
+// use Facebook\FacebookRequest;
+// use Facebook\GraphAlbum;
+// use Facebook\Entities\SignedRequest;
 
 
-class FacebookService extends FacebookSession {
+class FacebookService extends Facebook {
 	
 	const ALBUM_TYPE_PROFILE		= 'profile';
 	protected static $initialized	= false;
+	protected static $facebook;
 	
-	public function __construct($accessToken, SignedRequest $signedRequest = null) {
-		parent::__construct($accessToken, $signedRequest);
-		static::initialize();
+	public function __construct(array $config = array()) {
+		$config += array(
+			'app_id' => FACEBOOK_APP_ID,
+			'app_secret' => FACEBOOK_APP_SECRET,
+			'default_graph_version' => 'v2.2'
+		);
+		parent::__construct($config);
+// 		static::initialize();
 	}
+	
+	public function getAccessToken() {
+		return isset($_SESSION['fb_access_token']) ? $_SESSION['fb_access_token'] : null;
+	}
+	
+	public function isUserConnected() {
+		if( session_status() !== PHP_SESSION_ACTIVE ) {
+			throw new \Exception('Require the session started to check if user is connected');
+		}
+		return !!$this->getAccessToken();
+	}
+	
+	/**
+	 * 
+	 * @param string[]|string|null $fields
+	 * @return GraphUser
+	 */
+	public function getUser($fields=null) {
+		$accessToken = $this->getAccessToken();
+		if( !$accessToken ) {
+			throw new \Exception('User is not connected');
+		}
+		// Returns a `Facebook\FacebookResponse` object
+		$response = $this->get('/me'.($fields ? '?fields='.(is_array($fields) ? implode(',', $fields) : $fields) : ''), $accessToken);
+		
+		// 		$response = $FBService->get('/me?fields=id,name', $accessToken);
+		// 		var_dump($response);echo '<br>';
+		return $response->getGraphUser();
+		// 		var_dump($user);echo '<br>';
+// 		echo '
+// 		<p>Welcome '.$user->getName().'</p>';
+	}
+	
+	public function disconnectUser() {
+		if( session_status() !== PHP_SESSION_ACTIVE ) {
+			throw new \Exception('Require the session started to disconnect user');
+		}
+		unset($_SESSION['fb_access_token']);
+	}
+	
+	/**
+	 * @var boolean $connectedUser
+	 * 
+	 * True only if the use is just connnected
+	 */
+	protected $connectedUser = null;
+	public function connectUser() {
+		if( $this->connectedUser !== null ) {
+			return $this->connectedUser;
+		}
+		$this->connectedUser = false;
 
+		$helper = $this->getRedirectLoginHelper();
+		
+		$accessToken = $helper->getAccessToken();
+
+		if( $accessToken ) {
+			// Logged in
+			// 		echo '
+			// <h3>Facebook was just logged in</h3>
+			// <h6>Access Token</h6>';
+			// 		var_dump($accessToken->getValue());
+		
+			// The OAuth 2.0 client handler helps us manage access tokens
+			$oAuth2Client = $this->getOAuth2Client();
+		
+			// Get the access token metadata from /debug_token
+			$tokenMetadata = $oAuth2Client->debugToken($accessToken);
+			// 		echo '
+			// <h6>Metadata</h6>';
+			// 		var_dump($tokenMetadata);
+		
+			// Validation (these will throw FacebookSDKException's when they fail)
+			$tokenMetadata->validateAppId(FACEBOOK_APP_ID); // Replace {app-id} with your app id
+			// If you know the user ID this access token belongs to, you can validate it here
+			//$tokenMetadata->validateUserId('123');
+			$tokenMetadata->validateExpiration();
+		
+			if( !$accessToken->isLongLived() ) {
+				// 				echo 'Access token is not a long lived<br />';
+				// Exchanges a short-lived access token for a long-lived one
+// 				try {
+				$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+					// 					echo 'Got long lived access token<br />';
+// 				} catch (Facebook\Exceptions\FacebookSDKException $e) {
+// 					echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+// 					exit;
+// 				}
+		
+				// 			echo '
+				// <h6>Long-lived</h6>';
+				// 			var_dump($accessToken->getValue());
+				// 			} else {
+				// 				echo 'Access token IS a long lived<br>'.$accessToken.'<br>';
+			}
+			if( session_status() === PHP_SESSION_ACTIVE ) {
+				$_SESSION['fb_access_token'] = (string) $accessToken;
+			}
+			$this->connectedUser = true;
+			return $accessToken;
+		}
+		return false;
+	}
+	
+	public function getLoginUrl($redirectUrl, array $scope = array(), $separator = '&') {
+		$helper = $this->getRedirectLoginHelper();
+		return $helper->getLoginUrl($redirectUrl, $scope, $separator);
+	}
+	
+// 	public function __construct($accessToken = null, SignedRequest $signedRequest = null) {
+// 		parent::__construct($accessToken, $signedRequest);
+// 		static::initialize();
+// 	}
+
+	/*
 	public function getPhotosFromID($id) {
 		return $this->extractListAsStdClass(
 			new FacebookRequest($this, HTTP_METHOD_GET, '/'.$id.'/photos', array('fields'=>'id,images'))
@@ -38,7 +162,7 @@ class FacebookService extends FacebookSession {
 				$request = $input->getRequest();
 // 				debug('Request URL', FacebookRequest::BASE_GRAPH_URL . '/' . FacebookRequest::GRAPH_API_VERSION . $request->getPath());
 // 				debug('Raw response', $input->getRawResponse());
-				throw new Exception('Error getting response from facebook');
+				throw new \Exception('Error getting response from facebook');
 			}
 // 			die('Test');
 			$input	= $input->getGraphObjectList($type);
@@ -50,12 +174,22 @@ class FacebookService extends FacebookSession {
 		}
 		return $r;
 	}
+	
+	public static function getFacebook() {
+		return static::$facebook;
+	}
 
 	public static function initialize() {
 		if( static::$initialized ) { return; }
 		FacebookSession::setDefaultApplication(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET);
+		static::$facebook = new Facebook\Facebook([
+			'app_id' => FACEBOOK_APP_ID,
+			'app_secret' => FACEBOOK_APP_SECRET,
+			'default_graph_version' => 'v2.2',
+		]);
 		static::$initialized	= true;
 	}
+	*/
 }
 
 define('HTTP_METHOD_GET', 'GET');
