168f0e71b7fa7c13f13a46560053d1a93e3c5c17
Loenix <florent@sowapps.com>
2020-07-20 10:39:51 +0200

    Orpheus 3 upgrade compatibility Remove old eclipse things add compat with phpstorm Minor fixes Upgrade composer config, set to stable

--- src/Web/Facebook/Controller/AbstractFacebookLoginController.php
+++ src/Web/Facebook/Controller/AbstractFacebookLoginController.php
@@ -2,22 +2,22 @@
 
 namespace Orpheus\Web\Facebook\Controller;
 
+use Exception;
 use Facebook\Exceptions\FacebookResponseException;
 use Facebook\Exceptions\FacebookSDKException;
+use Facebook\GraphNodes\GraphUser;
 use Orpheus\InputController\HTTPController\HTTPController;
 use Orpheus\InputController\HTTPController\HTTPRequest;
 use Orpheus\InputController\HTTPController\RedirectHTTPResponse;
 use Orpheus\Web\Facebook\FacebookService;
-use Facebook\GraphNodes\GraphUser;
 
 abstract class AbstractFacebookLoginController extends HTTPController {
 	
 	/**
 	 * @param HTTPRequest $request The input HTTP request
-	 * @return HTTPResponse The output HTTP response
 	 * @see HTTPController::run()
 	 */
-	public function run(HTTPRequest $request) {
+	public function run($request) {
 		
 		try {
 			if( $request->hasParameter('error_code') ) {
@@ -26,52 +26,49 @@ abstract class AbstractFacebookLoginController extends HTTPController {
 				return $this->getErrorReponse($errorCode, $errorMessage);
 			}
 			
-			/* @var \Orpheus\Web\Facebook\FacebookService $FBService */
 			$fbService = new FacebookService();
 			
 			if( $fbService->connectUser() ) {
 				
-				/* @var \Facebook\GraphNodes\GraphUser $fbUser */
-				$fbUser	= $fbService->getUser('id,name,email,picture.type(large)');
-				$this->connectUser($fbUser);
+				$fbUser = $fbService->getUser('id,name,email,picture.type(large)');
+				$this->connectUser($fbService, $fbUser);
 				
-				return $this->getValidReponse($request);
+				return $this->getValidResponse($request);
 			}
 			
-		} catch(FacebookResponseException $e) {
+		} catch( FacebookResponseException $e ) {
 			// When Graph returns an error
-			echo 'Graph returned an error: '.$e;
+			echo 'Graph returned an error: ' . $e;
 			
-		} catch(FacebookSDKException $e) {
+		} catch( FacebookSDKException $e ) {
 			// When validation fails or other local issues
-			echo 'Facebook SDK returned an error: '.$e;
+			echo 'Facebook SDK returned an error: ' . $e;
 		}
 		
 		die();
 	}
 	
-	public abstract function connectUser(FacebookService $fbService, GraphUser $fbUser);
-	
 	public function getErrorReponse($errorCode, $errorMessage) {
-		echo 'Facebook Error ('.$errorCode.')<br />'.text2HTML($errorMessage);
+		echo 'Facebook Error (' . $errorCode . ')<br />' . text2HTML($errorMessage);
 		die();
 	}
 	
+	public abstract function connectUser(FacebookService $fbService, GraphUser $fbUser);
+	
 	/**
 	 * @param HTTPRequest $request The input HTTP request
-	 * @return HTTPResponse The output HTTP response
+	 * @return RedirectHTTPResponse
 	 * @see HTTPController::run()
 	 */
-	public function getValidReponse(HTTPRequest $request) {
+	public function getValidResponse($request) {
 		return new RedirectHTTPResponse(DEFAULTROUTE);
 	}
 	
 	/**
 	 * @param HTTPRequest $request The input HTTP request
-	 * @return HTTPResponse The output HTTP response
 	 * @see HTTPController::run()
 	 */
-	public function getInvalidReponse(HTTPRequest $request, \Exception $exception) {
+	public function getInvalidResponse($request, Exception $exception) {
 		return new RedirectHTTPResponse(DEFAULTROUTE);
 	}
 	
